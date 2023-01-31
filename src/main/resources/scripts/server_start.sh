c2dc6d5b45c347d7333fbf76a398f2c863d29c8c
bohdan-lutskyi <monster240798@gmail.com>
2023-01-31 16:00:04 +0200

    SP-develop fix for starting app on AWS

--- src/main/resources/scripts/server_start.sh
+++ src/main/resources/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/project/SombraPromotion || exit
-sudo java -Dspring.profiles.active=dev -jar promotion-0.0.1-SNAPSHOT.jar
+sudo java -Dspring.profiles.active=dev -jar promotion-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
