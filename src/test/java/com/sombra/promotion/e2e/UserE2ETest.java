bce45f9f2f623901b31f1cfe3e3e7ebb48a39a3c
Bohdan Lutskyi <monster240798@gmail.com>
2023-04-18 23:18:31 +0300

    SP-tests Added UserE2ETest

---
+++ src/test/java/com/sombra/promotion/e2e/UserE2ETest.java
@@ -0,0 +1,69 @@
+package com.sombra.promotion.e2e;
+
+import com.sombra.promotion.domain.Student;
+import com.sombra.promotion.domain.enumeration.UserRole;
+import com.sombra.promotion.e2e.abstraction.E2ETest;
+import com.sombra.promotion.util.TestUtil;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+public class UserE2ETest extends E2ETest {
+
+    @Test
+    void must_get_all_users() {
+        testUtil.createTestStudent();
+        testUtil.createTestInstructor();
+
+        given().header(CONTENT_TYPE, "application/json")
+                .header(AUTHORIZATION, testUtil.generateToken(TestUtil.DEFAULT_ADMIN_EMAIL, TestUtil.DEFAULT_PASSWORD))
+                .when()
+                .get(testURL("/users"))
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("", hasSize(3));
+    }
+    @Test
+    void instructor_can_not_get_all_users() {
+        testUtil.createTestStudent();
+        testUtil.createTestInstructor();
+
+        given().header(CONTENT_TYPE, "application/json")
+                .header(AUTHORIZATION, testUtil.generateToken(TestUtil.DEFAULT_INSTRUCTOR_EMAIL, TestUtil.DEFAULT_PASSWORD))
+                .when()
+                .get(testURL("/users"))
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    void must_assign_role() {
+        final Student testStudentWithoutRole = testUtil.createTestStudentWithoutRole();
+
+        given().header(CONTENT_TYPE, "application/json")
+                .header(AUTHORIZATION, testUtil.generateToken(TestUtil.DEFAULT_ADMIN_EMAIL, TestUtil.DEFAULT_PASSWORD))
+                .body(UserRole.STUDENT.name())
+                .when()
+                .put(testURL("/users/" + testStudentWithoutRole.getUser().getId() + "/role"))
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void instructor_can_not_assign_role() {
+        final Student testStudentWithoutRole = testUtil.createTestStudentWithoutRole();
+        testUtil.createTestInstructor();
+
+        given().header(CONTENT_TYPE, "application/json")
+                .header(AUTHORIZATION, testUtil.generateToken(TestUtil.DEFAULT_INSTRUCTOR_EMAIL, TestUtil.DEFAULT_PASSWORD))
+                .body(UserRole.STUDENT.name())
+                .when()
+                .put(testURL("/users/" + testStudentWithoutRole.getUser().getId() + "/role"))
+                .then()
+                .statusCode(403);
+    }
+}
